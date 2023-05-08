3b6006e9ba28d252f27370bd8590163c7a7a0931
Danik616 <ddanielvargas2011@gmail.com>
2023-05-08 17:08:29 -0500

    Se soluciona problema con el bean de securityContext

--- src/main/java/com/spring/prototipolitigando/security/SecurityConfiguration.java
+++ src/main/java/com/spring/prototipolitigando/security/SecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
 import org.springframework.web.server.ServerWebExchange;
 import com.spring.prototipolitigando.service.UserService;
 
@@ -22,7 +23,7 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecurityConfiguration{
+public class SecurityConfiguration {
 
     @Autowired
     private UserService userService;
@@ -37,25 +38,29 @@ public class SecurityConfiguration{
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
+    @Bean
+    public WebSessionServerSecurityContextRepository serverSecurityContextRepository() {
+        return new WebSessionServerSecurityContextRepository();
+    }
+
     @Bean
     public ReactiveAuthenticationManager reactiveAuthenticationManager() {
-        UserDetailsRepositoryReactiveAuthenticationManager authenticationManager =
-                new UserDetailsRepositoryReactiveAuthenticationManager(userService);
+        UserDetailsRepositoryReactiveAuthenticationManager authenticationManager = new UserDetailsRepositoryReactiveAuthenticationManager(
+                userService);
         authenticationManager.setPasswordEncoder(passwordEncoder());
         return authenticationManager;
     }
-    
+
     @Bean
     protected SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
         http.authorizeExchange()
-            .pathMatchers(HttpMethod.POST, "/login").permitAll()
-            .pathMatchers(HttpMethod.GET, "/logout").permitAll()
+                .pathMatchers(HttpMethod.POST, "/login").permitAll()
+                .pathMatchers(HttpMethod.GET, "/logout").permitAll()
                 .and()
                 .formLogin(login -> login
                         .loginPage("/login")
-                        .authenticationManager(reactiveAuthenticationManager())
-                        )
+                        .authenticationManager(reactiveAuthenticationManager()))
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessHandler((exchange, authentication) -> {
@@ -63,14 +68,12 @@ public class SecurityConfiguration{
                             response.setStatusCode(HttpStatus.SEE_OTHER);
                             response.getHeaders().setLocation(URI.create("/login"));
                             return Mono.empty();
-                         })
-                        )
-                        .exceptionHandling(handling -> handling.accessDeniedHandler(accessDeniedHandler))
-                        .csrf().disable();
-    
+                        }))
+                .exceptionHandling(handling -> handling.accessDeniedHandler(accessDeniedHandler))
+                .csrf().disable();
+
         http.addFilterAt(jwtRequestFilter, SecurityWebFiltersOrder.AUTHENTICATION);
         return http.build();
     }
 
-    
 }
