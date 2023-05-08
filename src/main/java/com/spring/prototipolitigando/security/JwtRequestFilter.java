5ae7b498eb1f0812e2150ce9cfa5139349b32149
Danik616 <ddanielvargas2011@gmail.com>
2023-05-08 18:25:29 -0500

    Se hacen cambios en el filtro de jwt

--- src/main/java/com/spring/prototipolitigando/security/JwtRequestFilter.java
+++ src/main/java/com/spring/prototipolitigando/security/JwtRequestFilter.java
@@ -1,25 +1,16 @@
 package com.spring.prototipolitigando.security;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-
-import java.security.Key;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
+import com.spring.prototipolitigando.exceptions.UnauthorizedException;
+import com.spring.prototipolitigando.service.JwtUtil;
 import reactor.core.publisher.Mono;
 
 
@@ -28,48 +19,37 @@ public class JwtRequestFilter implements WebFilter {
 @Value("${jwt.secret}")
 private String jwtSecret;
 
-private final ReactiveUserDetailsService userDetailsService;
-private final ServerSecurityContextRepository securityContextRepository;
-
-public JwtRequestFilter(ReactiveUserDetailsService userDetailsService,
-                        ServerSecurityContextRepository securityContextRepository) {
-    this.userDetailsService = userDetailsService;
-    this.securityContextRepository = securityContextRepository;
+private final JwtUtil jwtUtil;
+    
+public JwtRequestFilter(JwtUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
 }
 
 @Override
 public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-    final String authorizationHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
-
-    String username = null;
-    String jwt = null;
-
-    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-        jwt = authorizationHeader.substring(7);
-        try {
-            Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
-            JwtParser jwtParser = Jwts.parserBuilder().setSigningKey(key).build();
-            Claims claims = jwtParser.parseClaimsJws(jwt).getBody();
-            username = claims.getSubject();
-        } catch (ExpiredJwtException e) {
-            return exchange.getResponse().setComplete()
-    .onErrorResume(ExpiredJwtException.class, ex -> {
-        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-        return Mono.empty();
-    });
-        }
-    }
-
-    if (username != null) {
-        return this.userDetailsService.findByUsername(username)
-                .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()))
-                .flatMap(authentication -> {
-                    SecurityContext context = new SecurityContextImpl(authentication);
-                    return this.securityContextRepository.save(exchange, context);
-                })
-                .flatMap(v -> chain.filter(exchange));
+    String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+    
+    if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
+        String token = authorizationHeader.substring(7);
+        
+        return jwtUtil.validateToken(token)
+            .flatMap(isValid -> {
+                if (isValid) {
+                    String username = jwtUtil.extractUsername(token);
+                    return jwtUtil.generateToken(username)
+                        .flatMap(newToken -> {
+                            ServerHttpRequest request = exchange.getRequest().mutate()
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + newToken)
+                                .build();
+                            ServerWebExchange newExchange = exchange.mutate().request(request).build();
+                            return chain.filter(newExchange);
+                        });
+                } else {
+                    return Mono.error(new UnauthorizedException("Invalid token"));
+                }
+            });
     } else {
-        return chain.filter(exchange);
+        return Mono.error(new UnauthorizedException("Authorization header not found"));
     }
 }
 }
